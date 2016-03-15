c7bbcd548aa15d2627b29697b979867052efa05e
smicl11 <bryan.smith094@gmail.com>
2016-03-15 16:56:25 -0700

    formatted answers to the homework

--- functions.js
+++ functions.js
@@ -1,1 +1,35 @@
 // TODO: add your solutions here!
+// 1. Build your own concatentation
+
+
+
+// 2. Repeat a phrase
+
+
+
+// 3. Build your own Power function
+
+
+
+// 4. Area of a circle: π r2
+
+
+
+// 5. Pythagorean Theorem: a2 + b2 = c2
+
+
+
+// 6. Is X Evenly Divisible by Y ?
+
+
+
+// 7. Vowel Count:
+
+
+
+// 8. Build an ASCII Triangle!
+
+
+
+// 9. Stretch Challenge: Can you alter the printTriangle function to create a Pyramid?
+// LOL, no.
